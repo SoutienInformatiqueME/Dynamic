a47abf9edd60033d7991d04c36b9f8ef1fb8c8a0
SoutienInformatiqueME <soutieninformatique@moissonestrie.com>
2025-07-15 16:59:45 -0400

    Initial

---
+++ my-vuetify-app/src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Vuetify imports
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Vuetify instance
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+const app = createApp(App)
+app.use(vuetify)
+app.mount('#app')
